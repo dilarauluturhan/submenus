02021f6e8f84916a0d94e97781de1d90834d4d58
Dilara Uluturhan <dilarauluturhan@outlook.com>
2023-06-04 01:51:11 +0300

    Hero completed

--- src/Hero.js
+++ src/Hero.js
@@ -3,11 +3,28 @@ import phoneImg from './images/phone.svg';
 import { useGlobalContext } from './context.js';
 
 const Hero = () => {
-  const data = useGlobalContext();
-  console.log(data);
+  const { closeSubmenu } = useGlobalContext();
 
   return (
-    <div>Hero</div>
+    <>
+      <section className='hero'>
+        <div className='hero-center'>
+          <article className='hero-info'>
+            <h1>Payments infrastructure <br />
+              for the internet</h1>
+            <p>
+              Millions of companies of all sizes—from startups to Fortune 500s—use
+              Stripe’s software and APIs to accept payments, send payouts, and
+              manage their businesses online.
+            </p>
+            <button className='btn'>Start now</button>
+          </article>
+          <article className='hero-images'>
+            <img src={phoneImg} className='phone-img' alt='phone' />
+          </article>
+        </div>
+      </section>
+    </>
   )
 }
 
