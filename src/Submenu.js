f4b37703e3db1200d6bd9400dab9f28ba92d695a
Dilara Uluturhan <dilarauluturhan@outlook.com>
2023-06-04 22:08:45 +0300

    Submenu Links

--- src/Submenu.js
+++ src/Submenu.js
@@ -2,7 +2,7 @@ import React, { useState, useRef, useEffect } from 'react';
 import { useGlobalContext } from './context.js';
 
 const Submenu = () => {
-    const { isSubmenuOpen, location } = useGlobalContext();
+    const { isSubmenuOpen, location, page: { page, links } } = useGlobalContext();
     const container = useRef(null);
     useEffect(() => {
         const submenu = container.current;
@@ -11,7 +11,18 @@ const Submenu = () => {
         submenu.style.top = `${bottom}px`;
     }, [location])
     return (
-        <aside className={`${isSubmenuOpen ? 'submenu show' : 'submenu'}`} ref={container}>Submenu</aside>
+        <aside className={`${isSubmenuOpen ? 'submenu show' : 'submenu'}`} ref={container}>
+            <h4>{page}</h4>
+            <div className={`submenu-center col-2`}>
+                {links.map((link, index) => {
+                    const { label, icon, url } = link
+                    return <a key={index} href={url}>
+                        {icon}
+                        {label}
+                    </a>
+                })}
+            </div>
+        </aside>
     )
 }
 
--- src/context.js
+++ src/context.js
@@ -10,45 +10,30 @@ const AppContext = React.createContext();
 export const AppProvider = ({ children }) => {
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
     const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
-    const [location, setLocation] = useState({})
+    const [location, setLocation] = useState({});
+    const [page, setPage] = useState({ page: '', links: [] });
 
-    /*
-    openSidebar fonksiyonu, setIsSidebarOpen fonksiyonunu çağırarak isSidebarOpen durum değişkenini true olarak günceller.
-    */
     const openSidebar = () => {
         setIsSidebarOpen(true);
     };
 
-    /*
-    closeSidebar fonksiyonu, setIsSidebarOpen fonksiyonunu çağırarak isSidebarOpen durum değişkenini false olarak günceller.
-    */
     const closeSidebar = () => {
         setIsSidebarOpen(false);
     };
 
-    /*
-    openSubmenu fonksiyonu, setIsSubmenuOpen fonksiyonunu çağırarak isSubmenuOpen durum değişkenini true olarak günceller.
-    */
     const openSubmenu = (text, coordinates) => {
+        const page = sublinks.find((link) => link.page === text);
+        setPage(page);
         setLocation(coordinates);
         setIsSubmenuOpen(true);
     };
 
-    /*
-    closeSubmenu fonksiyonu, setIsSubmenuOpen fonksiyonunu çağırarak isSubmenuOpen durum değişkenini false olarak günceller.
-    */
     const closeSubmenu = () => {
         setIsSubmenuOpen(false);
     };
 
     return (
         <>
-            {/*
-        AppContext, bir bileşen ağacının belirli bir yerindeki component'lar arasında veri paylaşımını sağlamak için kullanılır.
-        Provider bileşeni, bu Context nesnesini kullanarak paylaşılacak verileri sağlar.
-        value özelliği bağlamda paylaşmak istediğimiz değerleri belirtir.
-        Bu değerler bağlamı kullanan alt bileşenler tarafından erişilebilir hale gelir.
-        */}
             <AppContext.Provider
                 value={{
                     isSubmenuOpen,
@@ -57,22 +42,16 @@ export const AppProvider = ({ children }) => {
                     openSidebar,
                     closeSubmenu,
                     closeSidebar,
-                    location
+                    location,
+                    page
                 }}
             >
-                {/* Veriye erişilebilen bileşenler */}
                 {children}
             </AppContext.Provider>
         </>
     )
 }
 
-/*
-useGlobalContext adında bir özel bir hook oluşturdum. Bu hook useContext hook'unu kullanarak AppContext bağlamına erişim sağlar.
-useGlobalContext hook'u çağrıldığında useContext(AppContext) ifadesi kullanılır 
-ve AppContext bağlamının değerlerini döndürür.
-Bu sayede, AppContext.Provider bileşeni tarafından sağlanan değerlere erişim sağlayan bileşenler, useGlobalContext hook'unu kullanarak bu değerlere kolayca erişebilirler.
-*/
 export const useGlobalContext = () => {
     return useContext(AppContext)
 }
